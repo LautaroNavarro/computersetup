1ede1c1e7fc203909a4763d3b955cabfd241dd3f
LautaroNavarro <navarro_lautaro@hotmail.com>
2021-03-09 16:15:47 -0300

    Adding scripts

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,60 +1,59 @@
-# [BREW INSTALLATION]
+#!/bin/bash
 
+# [BREW INSTALLATION]
 if ! which brew; then
     echo 'Installing brew, this will only be required the first time'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-
 # [BREW INSTALLATION END]
 
-
 # [SUBLIME TEXT INSTALLATION]
-
 if ! which subl; then
     echo 'Installing sublime text, this will only be required the first time'
     brew cask install sublime-text
 fi
-
 # [SUBLIME TEXT INSTALLATION END]
 
-# [GIT INSTALLATION]
+# [VIRTUALENV INSTALLATION]
+if ! which virtualenv; then
+    echo 'Installing virtualenv, this will only be required the first time'
+    python -m pip install --user virtualenv
+fi
+# [VIRTUALENV INSTALLATION END]
 
-if ! which subl; then
+# [GIT INSTALLATION]
+if ! which git; then
     echo 'Installing git, this will only be required the first time'
     brew install git
 fi
-
 # [GIT INSTALLATION END]
-
 
 # [BASH GIT PROMPT SECTION]
 # This snippet will give us git information when bash open in git repository
 # More information: https://github.com/magicmonty/bash-git-prompt
-
 if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 fi
-
-GIT_PROMPT_ONLY_IN_REPO=1
-source $HOME/.bash-git-prompt/gitprompt.sh
-
+echo $'\n# [BASH GIT PROMPT SECTION]' >> ~/.bash_profile
+echo 'GIT_PROMPT_ONLY_IN_REPO=1' >> ~/.bash_profile
+echo 'source $HOME/.bash-git-prompt/gitprompt.sh'  >> ~/.bash_profile
+echo $'# [BASH GIT PROMPT SECTION END]\n' >> ~/.bash_profile
 # [BASH GIT PROMPT SECTION END]
+
+# [COPY BINARIES]
+cp -r ./bin ~/
+chmod -R +x .
+# [COPY BINARIES END]
 
 
 # [ADD PATHs]
-
-PATH=$PATH:$HOME/bin
-
+echo 'PATH=$PATH:$HOME/bin' >> ~/.bash_profile
 # [ADD PATHs END]
 
 # [LOAD ALIASES]
-
-source ~/.bash_aliases
-
+echo 'source ~/.bash_aliases' >> ~/.bash_profile
 # [LOAD ALIASES END]
 
 # [LOAD SCRIPTS]
-
-source ~/.bash_scripts
-
+echo 'source ~/.bash_scripts' >> ~/.bash_profile
 # [LOAD SCRIPTS END]
